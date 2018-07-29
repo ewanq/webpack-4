11e69d82a9ed0b75af7cf484b0bd07dc544a94e4
ewanq <ewanoo@163.com>
2018-07-30 01:31:31 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+  alert('函数调用成功');
+}
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  output: {
+    path: __dirname,
+    filename: './dist/main.js',
+    library: 'pass',
+    libraryTarget: 'umd'
+  }
+}
